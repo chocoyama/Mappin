c93f6e9851b70022ff6ce41f1420c97ef3cd9aae
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2019-11-30 13:18:13 +0900

    なんとなくのデザイン

--- Mappin/View/ContentView.swift
+++ Mappin/View/ContentView.swift
@@ -32,14 +32,16 @@ struct ContentView: View {
                         .frame(width: 20, height: 20)
                         .padding(12.0)
                         .background(Color.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
                         .shadow(color: .gray, radius: 0.2, x: 1, y: 1)
                 }
                 .padding(.top, 16)
                 .padding(.trailing, 16)
             }
             
-            MapMenuView()
+            SemiModalView {
+                MapMenuView()
+            }
         }.gesture(
             DragGesture()
                 .onChanged { _ in
@@ -49,40 +51,62 @@ struct ContentView: View {
     }
 }
 
-struct MapMenuView: View {
+struct SemiModalView<Content>: View where Content: View {
     @State var dragOffset: CGFloat = .leastNormalMagnitude
-    private let searchQuery = PassthroughSubject<String, Never>()
+    
+    private let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8.0) {
-            SearchBarView(text: searchQuery, placeholder: "場所または住所を検索します")
-            HStack {
-                Text("Collection")
-            }
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 16.0) {
-                    ForEach(0..<20) { number in
-                        Circle()
-                            .frame(width: 60, height: 60)
-                    }
-                }
-            }
-        }
-        .padding([.top, .bottom], 8.0)
-        .background(Color.white)
-        .offset(x: 0, y: dragOffset)
-        .gesture(
-            DragGesture(coordinateSpace: .global)
-                .onChanged { (value) in
-                    self.dragOffset = value.translation.height
-                    print("### \(self.dragOffset)")
+        content()
+            .padding([.top, .bottom], 8.0)
+            .background(Color.white)
+            .offset(x: 0, y: dragOffset)
+            .gesture(
+                DragGesture(coordinateSpace: .global)
+                    .onChanged { (value) in
+                        self.dragOffset = value.translation.height
+                        print("### \(self.dragOffset)")
                 }
                 .onEnded { (value) in
                     withAnimation(Animation.spring()) {
                         self.dragOffset = 0.0
                     }
                 }
-        )
+            )
+    }
+}
+
+struct MapMenuView: View {
+    private let searchQuery = PassthroughSubject<String, Never>()
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 8.0) {
+            RoundedRectangle(cornerRadius: 8.0)
+                .fill(Color.gray)
+                .frame(width: 40, height: 4)
+            
+            SearchBarView(text: searchQuery, placeholder: "場所または住所を検索します")
+                .padding([.leading, .trailing], 8.0)
+            
+            HStack {
+                Text("Collection")
+                Spacer()
+            }.padding([.leading, .trailing], 16.0)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0.0) {
+                    ForEach(0..<20) { number in
+                        Circle()
+                            .frame(width: 60, height: 60)
+                            .padding(8.0)
+                    }
+                }.padding([.leading, .trailing], 8.0)
+            }
+        }
     }
 }
 
